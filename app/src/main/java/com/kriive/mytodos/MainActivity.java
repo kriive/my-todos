37368115b799813d8974e2ef2847d128118d2795
Manuel Romei <manuel.romei@raytecvision.com>
2020-09-14 11:38:28 +0200

    Add dialog

--- app/src/main/java/com/kriive/mytodos/MainActivity.java
+++ app/src/main/java/com/kriive/mytodos/MainActivity.java
@@ -1,16 +1,20 @@
 package com.kriive.mytodos;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 public class MainActivity extends AppCompatActivity {
     private final Integer SECOND_ACTIVITY_REQUEST_CODE = 1337;
@@ -20,23 +24,37 @@ public class MainActivity extends AppCompatActivity {
     private IllustrationFragment illustration = new IllustrationFragment();
 
     public void showIllustration() {
-        FrameLayout frag = (FrameLayout) findViewById (R.id.fragment);
-        frag.setVisibility (View.GONE); // or View.INVISIBLE, depending on what you exactly want
+        FrameLayout frag = (FrameLayout) findViewById(R.id.fragment);
+        frag.setVisibility(View.GONE);
 
-        FrameLayout ill = (FrameLayout) findViewById (R.id.illustration);
-        ill.setVisibility (View.VISIBLE); // or View.INVISIBLE, depending on what you exactly want
+        FrameLayout ill = (FrameLayout) findViewById(R.id.illustration);
+        ill.setVisibility(View.VISIBLE);
 
         getSupportFragmentManager().beginTransaction().replace(R.id.illustration, illustration).addToBackStack(null).commit();
     }
 
     public void showList() {
-        FrameLayout frag = (FrameLayout) findViewById (R.id.fragment);
-        frag.setVisibility (View.VISIBLE); // or View.INVISIBLE, depending on what you exactly want
+        FrameLayout frag = (FrameLayout) findViewById(R.id.fragment);
+        frag.setVisibility(View.VISIBLE);
 
-        FrameLayout ill = (FrameLayout) findViewById (R.id.illustration);
-        ill.setVisibility (View.GONE); // or View.INVISIBLE, depending on what you exactly want
+        FrameLayout ill = (FrameLayout) findViewById(R.id.illustration);
+        ill.setVisibility(View.GONE);
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment, fragment).addToBackStack(null).commit();
+    }
+
+    protected void showOrderBy() {
+        String[] singleItems = {"Creation time", "Category", "Due date"};
+
+        new MaterialAlertDialogBuilder(this)
+                .setTitle("Order by")
+                .setSingleChoiceItems(singleItems, 0, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.d("Selected", String.valueOf(which));
+                        dialog.dismiss();
+                    }
+                }).show();
     }
 
     @Override
@@ -44,7 +62,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mContext = getApplicationContext();
+        mContext = this;
+
+        MaterialToolbar myToolbar = (MaterialToolbar) findViewById(R.id.topAppBar);
+        myToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.order:
+                        showOrderBy();
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
 
         ExtendedFloatingActionButton fabExt = (ExtendedFloatingActionButton) findViewById(R.id.create_task_fab);
         fabExt.setOnClickListener(new View.OnClickListener() {
@@ -71,10 +103,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        // check that it is the SecondActivity with an OK result
         if (requestCode == SECOND_ACTIVITY_REQUEST_CODE) {
             if (resultCode == RESULT_OK) {
-                // get String data from Intent
                 ToDo todo = (ToDo) data.getSerializableExtra(Intent.EXTRA_TEXT);
                 if (todo != null) {
                     ToDoManager.getInstance().addToDo(todo);
